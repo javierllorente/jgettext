2468ff2f49ea7e34e043b19fa79131d6bb0b948b
Javier Llorente <javier@opensuse.org>
2022-01-15 16:40:34 +0100

    Create TranslationParserFactory interface, make ParserFactory implement it

--- src/main/java/com/javierllorente/jgettext/TranslationParserFactory.java
+++ src/main/java/com/javierllorente/jgettext/TranslationParserFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Javier Llorente <javier@opensuse.org>
+ * Copyright (C) 2022 Javier Llorente <javier@opensuse.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,26 +16,12 @@
  */
 package com.javierllorente.jgettext;
 
-import com.javierllorente.jgettext.exception.UnsupportedFileFormatException;
-
 /**
  *
  * @author javier
  */
-public class ParserFactory implements TranslationParserFactory {
-
-    @Override
-    public TranslationParser getParser(String fileFormat) {
-        if (fileFormat == null) {
-            return null;
-        }
-        
-        if (fileFormat.equalsIgnoreCase("po")) {
-            return new PoParser();
-        } else if(fileFormat.equalsIgnoreCase("json")) {
-            return new JsonParser();
-        } else {
-            throw new UnsupportedFileFormatException(fileFormat);
-        }
-    }
+public interface TranslationParserFactory {
+    
+    public TranslationParser getParser(String fileFormat);
+    
 }
